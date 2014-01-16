2d2216333ce993f89cf05c9127470e9b8fdd4398
Damon <dtoumbourou@gmail.com>
2014-01-16 15:28:03 +1100

    commit

---
+++ display.py
@@ -0,0 +1,8 @@
+import xbmc, xbmcgui
+/Users/damon/Library/Application Support/XBMC/addons/plugin.audio.abc-radio-national/display.py
+class MyClass(xbmcgui.Window):
+    print 'hello world'
+
+mydisplay = MyClass()
+mydisplay .doModal()
+del mydisplay
